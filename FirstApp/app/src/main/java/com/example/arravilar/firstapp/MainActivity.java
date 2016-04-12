85f24400a6d2c3d6b79b1f2668fc3f34449419cb
Przemysław Lewandowski <przemo1114@op.pl>
2016-04-12 19:16:51 +0200

    Szablon

--- FirstApp/app/src/main/java/com/example/arravilar/firstapp/MainActivity.java
+++ FirstApp/app/src/main/java/com/example/arravilar/firstapp/MainActivity.java
@@ -3,12 +3,8 @@ package com.example.arravilar.firstapp;
 import android.location.Location;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.View;
-
-
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
-import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
@@ -35,16 +31,19 @@ public class MainActivity extends Activity {
             @Override
             public void onMapReady(final MapboxMap mapboxMap) {
                 mapboxMap.setMyLocationEnabled(true);
+                mapboxMap.setMinZoom(20);
+                mapboxMap.setMaxZoom(15);
                 mapboxMap.setOnMyLocationChangeListener(new MapboxMap.OnMyLocationChangeListener()
                 {
                     @Override
                     public void onMyLocationChange(Location location)
                     {
                         CameraPosition cameraPosition = new CameraPosition.Builder()
-                                .target(new LatLng(gpsLocation.getLocation())) // set the camera's center position
-                                .zoom(25)  // set the camera's zoom level
+                                .target(new LatLng(mapboxMap.getMyLocation())) // set the camera's center position
+                                .zoom(mapboxMap.getCameraPosition().zoom)  // set the camera's zoom level
                                 .tilt(20)  // set the camera's tilt
                                 .build();
+
                                 mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
                     }
 

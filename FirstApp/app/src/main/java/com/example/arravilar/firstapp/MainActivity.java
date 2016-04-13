45d02c325e936c5fdd236ab2c8586dfaa21ed9d0
Przemysław Lewandowski <przemo1114@op.pl>
2016-04-13 09:44:37 +0200

    adawdawdaw

--- FirstApp/app/src/main/java/com/example/arravilar/firstapp/MainActivity.java
+++ FirstApp/app/src/main/java/com/example/arravilar/firstapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.arravilar.firstapp;
 
+import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
 import android.app.Activity;
@@ -13,10 +14,13 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 public class MainActivity extends Activity {
 
     private MapView mapView;
-
-    @Override
+    private RouteList routeList;
+    private Context context;
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+            context = this.context;
+        routeList = null;
         setContentView(R.layout.activity_main);
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
@@ -32,12 +36,15 @@ public class MainActivity extends Activity {
                     @Override
                     public void onMyLocationChange(Location location)
                     {
+                     if (GlobalValues.getInstance().getRecordRoute() == true) {
+                            if (routeList == null) routeList = new RouteList(context);
+                         routeList.getRoutes().get(routeList.getRouteNumber()-1).addPoint(new LatLng(mapboxMap.getMyLocation()));
+                     }
                         CameraPosition cameraPosition = new CameraPosition.Builder()
                                 .target(new LatLng(mapboxMap.getMyLocation())) // set the camera's center position
                                 .zoom(mapboxMap.getCameraPosition().zoom)  // set the camera's zoom level
                                 .tilt(mapboxMap.getCameraPosition().tilt)  // set the camera's tilt
                                 .build();
-
                                 mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
                     }
 

a21c045674287213cabca234c88e55bc44b50502
Mendrzec <projektsage@gmail.com>
2016-04-13 10:02:30 +0200

    no message

--- FirstApp/app/src/main/java/com/example/arravilar/firstapp/Main2Activity.java
+++ FirstApp/app/src/main/java/com/example/arravilar/firstapp/Main2Activity.java
@@ -3,6 +3,8 @@ package com.example.arravilar.firstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
@@ -18,5 +20,7 @@ public class Main2Activity extends AppCompatActivity {
         txtSaveRoute.setText(routeName);
     }
 
-    
+    public void saveClick(View v){
+        EditText txtSaveRoute =
+    }
 }
